343c813881f68ace26434e8c5f01ca904647d6e7
hayato423 <hayato0423terao@gmail.com>
2021-03-18 19:07:38 +0900

    小数点での改行を除去． 段落はじめに空白を挿入．

--- convertForDeepL.py
+++ convertForDeepL.py
@@ -1,11 +1,12 @@
 import pyperclip
 clip_str = pyperclip.paste()
-strings = clip_str.split(".")
-strings.pop(len(strings)-1)
+lines = clip_str.splitlines()
 result_str = ""
-for s in strings:
-    s = s.replace("\r\n","")
-    s = s + ".\r\n"
-    result_str += s
-    print(s,end="")
+for l in lines:    
+    if l[-1] == ".":
+        result_str += l
+        result_str += "\r\n    "
+    else:
+        result_str += l        
+
 pyperclip.copy(result_str)
